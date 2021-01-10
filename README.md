e9f2f2eb7c1f47f9cb3762d7b34ea127f66d1d6a
zkan <zkan.cs@gmail.com>
2021-01-10 11:52:11 +0700

    Implement count vowels :bear:

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ```sh
 cc -o leap leap.c
 ```
+
+```sh
+gcc count_vowel.c -o count_vowel.c
+./count_vowels "EnginEer"
+```
---
+++ count_vowels.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int is_vowel(char c) {
+    c = tolower(c);
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+
+int count_vowels(char *word) {
+    int count = 0;
+    int len = strlen(word);
+    for(int i = 0; i < len; ++i) {
+        char c = word[i];
+        if(is_vowel(c)) {
+            ++count;
+        }
+    }
+    return count;
+}
+
+void test_count_vowels() {
+    printf("%d\n", count_vowels("rawitat"));
+    printf("%d\n", count_vowels("engineer"));
+    printf("%d\n", count_vowels("EngIneEr"));
+}
+
+int main(int argc, char* argv[]) {
+    printf("%d\n", count_vowels(argv[1]));
+
+    return 0;
+}
